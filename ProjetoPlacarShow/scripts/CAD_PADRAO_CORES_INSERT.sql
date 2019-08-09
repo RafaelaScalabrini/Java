8828a2bd6b26dbb04773cb577bd238c604f3260c
Rafaela Scalabrini <rafaelascalabrini152@gmail.com>
2019-08-08 23:40:09 -0300

    Versoesfinais

--- ProjetoPlacarShow/scripts/CAD_PADRAO_CORES_INSERT.sql
+++
@@ -1,5 +0,0 @@
-USE DBPLACAR
-
-IF NOT EXISTS(SELECT * FROM CAD_PADRAO_CORES WHERE PADRAO_COR_NOME = 'ORIGINAL')
-	INSERT INTO CAD_PADRAO_CORES (padrao_cor_nome,fundopainel_cor,textos_cor,fundocontadores_cor,contadores_cor)
-	VALUES ('ORIGINAL','&H0&','&HFFFFFF','&H404040','&HFF&')
